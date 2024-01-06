c9052a6935a6ef01d7d7205dbf93f4aa39ec3eb3
Robert Teets <teetsrobert@gmail.com>
2024-01-05 19:11:53 -0700

    feat: built out the apps full functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import axios from "axios";
 import "./App.css";
 import divider from "./assets/pattern-divider-desktop.svg";
 import dice from "./assets/icon-dice.svg";
 
+interface IAdviceSlip {
+    id: number;
+    advice: string;
+}
+
 function App() {
+    const [adviceObject, setAdviceObject] = useState<IAdviceSlip>({
+        id: 0,
+        advice: "Click the dice to get some advice.",
+    });
+    const handleClick = (event: FormEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+
+        axios
+            .get("https://api.adviceslip.com/advice")
+            .then((response) => {
+                setAdviceObject(response.data.slip);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
     return (
         <div className="container">
-            <h1 className="quoteNumber">Advice</h1>
-            <p className="quote">
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-                eveniet placeat ab, minima adipisci et molestias? Impedit
-                officia necessitatibus fugiat in enim eos suscipit saepe iure,
-                harum ipsam distinctio veniam?"
-            </p>
+            <h1 className="quoteNumber">
+                {adviceObject.id == 0
+                    ? "ADVICE AWAITS"
+                    : "ADVICE #" + adviceObject.id}
+            </h1>
+            <p className="quote">{adviceObject.advice}</p>
             <img src={divider} alt="graphical divider" />
-            <button className="diceButton">
+            <button onClick={handleClick} className="diceButton">
                 <img src={dice} alt="dice button" />
             </button>
         </div>
